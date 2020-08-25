7e4f8f437bf7124b88c79ffdd99209ec487b8b7f
Flavia Fukuda <flavia.noriko.work@gmail.com>
2020-08-25 10:33:24 -0300

    Alteração no caminho da entity

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -19,21 +19,21 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-// app.use(
-//   (err: Error, request: Request, response: Response, next: NextFunction) => {
-//     if (err instanceof AppError) {
-//       return response.status(err.statusCode).json({
-//         status: 'error',
-//         message: err.message,
-//       });
-//     }
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
 
-//     return response.status(500).json({
-//       status: 'error',
-//       message: 'Internal server error',
-//     });
-//   },
-// ); // aqui!!
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+); // aqui!!
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');

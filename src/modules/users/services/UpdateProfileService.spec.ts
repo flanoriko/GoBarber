17ca0dc2aeffdced194b4c455834f531910d3de0
Flavia Fukuda <flavia.noriko.work@gmail.com>
2020-08-31 18:50:43 -0300

    Update Profile

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -35,4 +35,92 @@ describe('UpdateProfile', () => {
     expect(updatedUser.name).toBe('John');
     expect(updatedUser.email).toBe('john@gmail.com');
   });
+
+  // categorizando o tipo com o describe
+  it('should not change to another user email', async () => {
+    await fakeUsersRepository.create({
+      name: 'Flavia',
+      password: '123',
+      email: 'flavia@gmail.com',
+    });
+    const user = await fakeUsersRepository.create({
+      name: 'Andre',
+      password: '1456423',
+      email: 'andre@gmail.com',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        name: 'John',
+        email: 'flavia@gmail.com',
+        // password: (await user).password,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('user does not exist!', async () => {
+    await expect(
+      updateProfileService.execute({
+        user_id: 'fake_id',
+        name: 'John',
+        email: 'flavia@gmail.com',
+        // password: (await user).password,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to update the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Flavia',
+      password: '123',
+      email: 'flavia@gmail.com',
+    });
+
+    const updatedUser = await updateProfileService.execute({
+      user_id: user.id,
+      name: 'John',
+      old_password: '123',
+      password: '3333',
+      email: 'john@gmail.com',
+      // password: (await user).password,
+    });
+    expect(updatedUser.password).toBe('3333');
+  });
+
+  it('should not be able to update the password if old password is not informed', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Flavia',
+      password: '123',
+      email: 'flavia@gmail.com',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        name: 'John',
+        password: '3333',
+        email: 'john@gmail.com',
+        // password: (await user).password,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to update the password with wrong old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Flavia',
+      password: '123',
+      email: 'flavia@gmail.com',
+    });
+
+    const updatedUser = await updateProfileService.execute({
+      user_id: user.id,
+      name: 'John',
+      old_password: '454545',
+      password: '3333',
+      email: 'john@gmail.com',
+      // password: (await user).password,
+    });
+    expect(updatedUser.password).toBe('3333');
+  });
 });
--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 
 import AppErrors from '@shared/errors/AppError';
 
+import passwordRouter from '../infra/http/routes/password.routes';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 import IUsersRepository from '../repositories/IUserRepository';
 
@@ -30,17 +31,29 @@ class UpdateProfile {
     password,
   }: IRequest): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
+    const email_already_used_by_another_user = await this.usersRepository.findByEmail(
+      email,
+    );
 
+    if (
+      email_already_used_by_another_user &&
+      email_already_used_by_another_user.id !== user_id
+    ) {
+      throw new AppErrors('Email already in use!', 401);
+    }
     // validando se o usuario existe:
     if (!user) {
       throw new AppErrors('User is not registered', 401);
     }
-    if (
-      password &&
-      old_password &&
-      this.hashProvider.compareHash(user.password, old_password)
-    ) {
-      user.password = await this.hashProvider.generateHash(password);
+
+    if (password && !old_password) {
+      throw new AppErrors('Old Password must be informed', 401);
+    }
+
+    if (password && old_password) {
+      if (this.hashProvider.compareHash(user.password, old_password)) {
+        user.password = await this.hashProvider.generateHash(password);
+      }
     }
 
     user.name = name;
